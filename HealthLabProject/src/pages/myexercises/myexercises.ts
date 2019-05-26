dda65c93edee5436e18c407f9df77cc5e8f95054
s194157 <bartekodrobina@gmail.com>
2019-05-27 00:04:01 +0200

    Dodanie filmów

--- HealthLabProject/src/pages/myexercises/myexercises.ts
+++ HealthLabProject/src/pages/myexercises/myexercises.ts
@@ -15,7 +15,31 @@ export class MyExercisesPage {
     {
       title: '5 prostych ćwiczeń',
       video: 'https://www.youtube.com/embed/sDwxXZgfYfQ',
-    }
+    },
+    {
+      title: 'Trening brzucha',
+      video: 'https://www.youtube.com/embed/LKO1yzvtK9s',
+    },
+    {
+      title: 'Trening funkcjonalny',
+      video: 'https://www.youtube.com/embed/Die0oESBhyE',
+    },
+    {
+      title: 'Martwy ciąg',
+      video: 'https://www.youtube.com/embed/0_igODjLiXM',
+    },
+    {
+      title: 'Podciąganie',
+      video: 'https://www.youtube.com/embed/olxgbxHIHTY',
+    },
+    {
+      title: 'Wyciskanie sztangi',
+      video: 'https://www.youtube.com/embed/bbGuHx07EDc',
+    },
+    {
+      title: 'Przysiad',
+      video: 'https://www.youtube.com/embed/NEduXlZ8zSk',
+    },
   ]
 
   constructor(public navCtrl: NavController){
