e901e2a43341c4b16a63780b939605fa666d97fb
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-27 20:42:51 +0200

    do poprawek powiadomienia

--- HealthLabProject/src/pages/liquids/liquids.ts
+++ HealthLabProject/src/pages/liquids/liquids.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { LocalNotifications, LocalNotificationsOriginal } from "@ionic-native/local-notifications";
+import { NavController, NavParams, Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 
 @Component({
@@ -9,9 +9,29 @@ import { LocalNotifications, LocalNotificationsOriginal } from "@ionic-native/lo
 })
 export class LiquidsPage {
   
-  constructor(private localNotifications: LocalNotificationsOriginal) {
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private plt: Platform, public navParams: NavParams) {
+    this.plt.ready().then((rdy) ==> {
+      this.localNotifications.on('click', (notification,state) ==> {
+        let json = JSON.parse(notification, data);
+
+        let alert = this.alertCtrl.create({
+          title: Notification,title,
+          subTitle json.mydata
+        });
+        alert.present();
+      });
+    });
   }
 
+  scheduleNotification() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Liquid',
+      text: 'Napij sie wody!'
+      at: new Date(new Date().getTime() + 5 * 1000),
+      data: {mydata: 'wiecej tekstu'}
+    });
+  }
   
 
 }
