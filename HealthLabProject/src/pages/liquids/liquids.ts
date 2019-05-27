1fc9a95f80247e77c3a37d57efb0de957cd83f9a
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-27 23:31:05 +0200

    zakonczenie prac. problem z @ionic/angular

--- HealthLabProject/src/pages/liquids/liquids.ts
+++ HealthLabProject/src/pages/liquids/liquids.ts
@@ -1,76 +1,81 @@
+import { Platform, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController, NavParams, Platform, AlertController } from 'ionic-angular';
-import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
-
-
+import { LocalNotifications, ELocalNotificationTriggerUnit, ILocalNotificationActionType, ILocalNotification } from '@ionic-native/local-notifications/ngx';
+ 
 @Component({
-  selector: 'liquids',
-  templateUrl: 'liquids.html'
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class LiquidsPage {
+export class HomePage {
   scheduled = [];
-  
-  constructor(public alertCtrl: AlertController, 
-    private localNotifications: LocalNotifications,
-     private plt: Platform) {
-    this.plt.ready().then(() => {
+ 
+  constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController) {
+    /*this.plt.ready().then(() => {
       this.localNotifications.on('click').subscribe(res => {
-        console.log('click: ', res);
         let msg = res.data ? res.data.mydata : '';
         this.showAlert(res.title, res.text, msg);
       });
+ 
       this.localNotifications.on('trigger').subscribe(res => {
-        console.log('click: ', res);
         let msg = res.data ? res.data.mydata : '';
         this.showAlert(res.title, res.text, msg);
       });
     });
+    */
   }
-
+ 
   scheduleNotification() {
     this.localNotifications.schedule({
       id: 1,
-      title: 'cos tam',
-      text: 'cos tam2',
-      data: { mydata: 'cos specjalnego'},
+      title: 'Attention',
+      text: 'Simons Notification',
+      data: { mydata: 'My hidden message this is' },
       trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND },
-      foreground: true
+      foreground: true // Show the notification while app is open
     });
+ 
+    // Works as well!
+    // this.localNotifications.schedule({
+    //   id: 1,
+    //   title: 'Attention',
+    //   text: 'Simons Notification',
+    //   data: { mydata: 'My hidden message this is' },
+    //   trigger: { at: new Date(new Date().getTime() + 5 * 1000) }
+    // });
   }
-
+ 
   recurringNotification() {
     this.localNotifications.schedule({
       id: 22,
-      title: 'drugie',
-      text: 'drugie2',
+      title: 'Recurring',
+      text: 'Simons Recurring Notification',
       trigger: { every: ELocalNotificationTriggerUnit.MINUTE }
-      //foreground: true
     });
   }
-
-  repeatingDialy() {
+ 
+  repeatingDaily() {
     this.localNotifications.schedule({
       id: 42,
-      title: 'trzecie',
-      text: 'trzecie2',
-      trigger: { every: { hour: 11, minute: 50} },
+      title: 'Good Morning',
+      text: 'Code something epic today!',
+      trigger: { every: { hour: 9, minute: 30 } }
     });
   }
-
-  getAll() {
-    this.localNotifications.getAll().then(res => {
-      this.scheduled = res;
-    })
-  }
-
-  showAlert(header, sub, msg) {
+ 
+ /* showAlert(header, sub, msg) {
     this.alertCtrl.create({
-      header: header;
+      header: header,
       subHeader: sub,
-      message: msg, 
+      message: msg,
       buttons: ['Ok']
     }).then(alert => alert.present());
   }
-  
-
+  */
+ 
+  getAll() {
+    this.localNotifications.getAll().then((res: ILocalNotification[]) => {
+      this.scheduled = res;
+    })
+  }
 }
