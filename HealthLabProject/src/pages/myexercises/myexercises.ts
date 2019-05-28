61692c69b4325767a49ed1184adb458803a02246
devKomak <komcio96@gmail.com>
2019-05-28 12:42:55 +0200

    myexercises update

--- HealthLabProject/src/pages/myexercises/myexercises.ts
+++ HealthLabProject/src/pages/myexercises/myexercises.ts
@@ -10,35 +10,35 @@ export class MyExercisesPage {
   videos: any[] = [
     {
       title: 'Legs workout with Mariusz Pudzianowski',
-      video: 'https://www.youtube.com/watch?v=hEJ6HZX6pTY',
+      video: 'https://www.youtube.com/embed/hEJ6HZX6pTY',
     },
     {
-      title: '5 Simple Exercises to Lose Thigh Fat',
-      video: 'https://www.youtube.com/watch?v=-RfsfNsp1vs',
+      title: '5 Simple Exercises to Build Back',
+      video: 'https://www.youtube.com/embed/x9jAiBG--KA',
     },
     {
       title: '10 Min Abs Workout',
-      video: 'https://www.youtube.com/watch?v=1919eTCoESo',
+      video: 'https://www.youtube.com/embed/1919eTCoESo',
     },
     {
       title: 'Basic Functional Training',
-      video: 'https://www.youtube.com/watch?v=NX8lRmIsHLE',
+      video: 'https://www.youtube.com/embed/NX8lRmIsHLE',
     },
     {
       title: 'Exercises to strengthen back',
-      video: 'https://www.youtube.com/watch?v=TGI5TFnY8Ck',
+      video: 'https://www.youtube.com/embed/TGI5TFnY8Ck',
     },
     {
       title: 'Biceps Workout',
-      video: 'https://www.youtube.com/watch?v=gozU3CUIizs',
+      video: 'https://www.youtube.com/embed/gozU3CUIizs',
     },
     {
       title: 'Bench Press',
-      video: 'https://www.youtube.com/watch?v=vthMCtgVtFw',
+      video: 'https://www.youtube.com/embed/rT7DgCr-3pg',
     },
     {
-      title: 'Barbell Squat',
-      video: 'https://www.youtube.com/watch?v=1oed-UmAxFs',
+      title: 'Squat',
+      video: 'https://www.youtube.com/embed/bEv6CCg2BC8',
     },
   ]
 
