bccc5d851a0d69096b48455589b563c0ac63e119
Kasia <zdeb.katarzyna@gmail.com>
2019-05-29 00:26:45 +0200

    working api

--- HealthLabProject/src/pages/medicines/medicines.ts
+++ HealthLabProject/src/pages/medicines/medicines.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProvidersDrugsProvider} from '../../providers/providers-drugs/providers-drugs';
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import {Result} from '../../providers/providers-drugs/providers-drugs';
 // import { Injectable } from '@angular/core';
 import 'rxjs/Rx'; 
 
@@ -12,17 +14,40 @@ import 'rxjs/Rx';
 
 export class MedicinesPage implements OnInit {
 
-  results :any;
+  drug :any;
+  results: any;
   searchTerm= 'ibuprofen';
+  userList=[];
+  reported_effects_keys:any;
+  reported_effects: any;
+  something:  Observable<Result[]>
+
 
   constructor(public navCtrl: NavController, private drugsProvider: ProvidersDrugsProvider) {
   }
 
-  ngOnInit(){
-      this.drugsProvider.getDetails(this.searchTerm).subscribe(e =>{
-        this.results = e;
-        console.log('My result'+ this.results);
-      });
+  // ngOnInit(){
+  //     this.drugsProvider.getDetails(this.searchTerm).subscribe((res) => {
+  //       this.drug = res[ 'result' ][ 0 ];
+  //       this.reported_effects_keys = Object.keys(this.drug[ 'side_effects' ][ 'reported_effects' ]);
+  //       this.reported_effects = this.drug[ 'side_effects' ][ 'reported_effects' ];
+  //       console.log('My result'+ this.drug);
+  //     });
       
+  // }
+
+  ngOnInit(){
+    this.something = this.drugsProvider.getDetails(this.searchTerm).map(e => e);
+    console.log(this.something[0]);
+  }
+
+  search(){
+    let newea = this.drugsProvider.getDetails(this.searchTerm);
+    console.log(newea);
+
+    this.drugsProvider.getDetails(this.searchTerm).subscribe(e => {
+      this.results = e;
+      console.log('My result'+ this.results);
+    });
   }
 }
