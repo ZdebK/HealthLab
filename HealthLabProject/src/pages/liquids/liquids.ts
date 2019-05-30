34ecc0a0262bcd62dab0745e09c6ec3ec2328a49
Kasia <zdeb.katarzyna@gmail.com>
2019-05-31 00:49:50 +0200

    notification without yt - changes

--- HealthLabProject/src/pages/liquids/liquids.ts
+++ HealthLabProject/src/pages/liquids/liquids.ts
@@ -1,41 +1,60 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, Platform} from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 import { LocalNotifications } from '@ionic-native/local-notifications/index';
-
+ 
 @Component({
   selector: 'liquids',
   templateUrl: 'liquids.html'
 })
 export class LiquidsPage {
   data = { title:'', description:'', date:'', time:'' };
-
-  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private alert: AlertController,  private platform: Platform ){
+  dataDaily =  {description:'', day:'', time:'' };
+ 
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private alert: AlertController ){
   }
-
+ 
   submit() {
     console.log(this.data);
-    var date = new Date(this.data.date+" "+this.data.time);
+    var date = new Date(this.data.date + " " + this.data.time);
     console.log(date);
     this.localNotifications.schedule({
-       text: 'Delayed ILocalNotification',
+       text: "Drink glass of water :)\n" + this.data.description,
        trigger: {at: new Date(new Date().getTime() + 3600)},
        led: 'FF0000',
-       sound: this.setSound(),
+       sound: 'file://assets/sounds/water_message.mp3',
     });
     let alert = this.alert.create({
       title: 'Congratulation!',
-      subTitle: 'Notification setup successfully at '+date,
+      subTitle: 'Notification setup successfully at '+ date,
       buttons: ['OK']
     });
     alert.present();
     this.data = { title:'', description:'', date:'', time:'' };
   }
-
-  setSound() {
-    if (this.platform.is('android')) {
-      return 'file://assets/sounds/water_message.mp3'
-    } else {
-      return 'file://assets/sounds/water_message.caf'
-    }
+ 
+  singleUseNotificationWater(){
+    this.localNotifications.schedule({
+      text: "Drink glass of water :)\n" + this.data.description,
+      trigger: {at: new Date(new Date().getTime() + 3600)},
+      led: 'FF0000',
+      sound: 'file://assets/sounds/water_message.mp3',
+   });
+   this.data = { title:'', description:'', date:'', time:'' };
   }
+ 
+  // dailyNotificationWater(){
+  //   this.localNotifications.schedule({
+  //     id: 1,
+  //     title: "df",
+  //     foreground: true,
+  //     text: "Drink glass of water :)\n" + this.dataDaily.description,
+  //     trigger: { every: { hour: this.dataDaily.time, }, count: 1 },
+  //     sound: 'file://assets/sounds/water_message.mp3',
+  //  });
+  //  this.dataDaily = {description:'', day:'', time:'' };
+  // }
+ 
+    updateDay(day){
+      this.dataDaily.day = day;
+    }
 }
