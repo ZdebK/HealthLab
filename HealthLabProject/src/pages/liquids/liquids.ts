ace3300a4fe928d373be07be112454d0be2ef255
Kasia <zdeb.katarzyna@gmail.com>
2019-06-01 12:10:32 +0200

    notifivstions changes daily and single

--- HealthLabProject/src/pages/liquids/liquids.ts
+++ HealthLabProject/src/pages/liquids/liquids.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController} from 'ionic-angular';
-import { LocalNotifications } from '@ionic-native/local-notifications/index';
+import { NavController, AlertController, Platform} from 'ionic-angular';
+import { LocalNotifications, ELocalNotificationTriggerUnit} from '@ionic-native/local-notifications/index';
  
 @Component({
   selector: 'liquids',
@@ -8,24 +8,29 @@ import { LocalNotifications } from '@ionic-native/local-notifications/index';
 })
 export class LiquidsPage {
   data = { title:'', description:'', date:'', time:'' };
-  dataDaily =  {description:'', day:'', time:'' };
+  dataDaily =  {description:'', day:'',date: '' , time:'' };
+  sas : Date;
  
-  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private alert: AlertController ){
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private alert: AlertController, private platform: Platform){
   }
+
+
  
   submit() {
     console.log(this.data);
-    var date = new Date(this.data.date + " " + this.data.time);
-    console.log(date);
+    const date2 = new Date(this.data.date + " " + this.data.time);
+    console.log(date2);
+
+    
     this.localNotifications.schedule({
-       text: "Drink glass of water :)\n" + this.data.description,
+       text: "Drink glass of water :)" + this.data.description,
        trigger: {at: new Date(new Date().getTime() + 3600)},
        led: 'FF0000',
        sound: 'file://assets/sounds/water_message.mp3',
     });
     let alert = this.alert.create({
       title: 'Congratulation!',
-      subTitle: 'Notification setup successfully at '+ date,
+      subTitle: 'Notification setup successfully at '+ date2,
       buttons: ['OK']
     });
     alert.present();
@@ -33,28 +38,50 @@ export class LiquidsPage {
   }
  
   singleUseNotificationWater(){
+    const diffTime = Math.abs(new Date().getTime() - new Date((this.data.date)).getTime());
+    console.log(diffTime);
+    var yr = new Date(this.data.date).getFullYear();
+    console.log( yr);
+    var mnth = new Date(this.data.date).getMonth();
+    console.log(mnth);
+    var dat = new Date(this.data.date).getDate();
+    console.log(dat);
+    var currentDate = new Date();
+
+
+
+    var hour2 = this.data.time.split(":")[0];
+    var minute2 = this.data.time.split(":")[0];
     this.localNotifications.schedule({
+      id: 1, 
       text: "Drink glass of water :)\n" + this.data.description,
-      trigger: {at: new Date(new Date().getTime() + 3600)},
+      trigger: {at: new Date(new Date().getTime() + diffTime)},
       led: 'FF0000',
       sound: 'file://assets/sounds/water_message.mp3',
    });
    this.data = { title:'', description:'', date:'', time:'' };
   }
  
-  // dailyNotificationWater(){
-  //   this.localNotifications.schedule({
-  //     id: 1,
-  //     title: "df",
-  //     foreground: true,
-  //     text: "Drink glass of water :)\n" + this.dataDaily.description,
-  //     trigger: { every: { hour: this.dataDaily.time, }, count: 1 },
-  //     sound: 'file://assets/sounds/water_message.mp3',
-  //  });
-  //  this.dataDaily = {description:'', day:'', time:'' };
-  // }
+  dailyNotificationWater(){
+    console.log("Data" + new Date(this.dataDaily.date));
+    console.log("TIME" +   this.dataDaily.time);
+    console.log("TIME2" + this.dataDaily.time.split(":")[0]);
+    this.platform.ready().then(() => {
+    this.localNotifications.schedule({
+      id: 2,
+      text: "Drink glass of water :)\n" + this.dataDaily.description,
+      trigger: { firstAt : new Date(), every: ELocalNotificationTriggerUnit.MINUTE },
+      led: 'FF0000',
+      sound: 'file://assets/sounds/water_message.mp3',
+   });
+  });
+}
+   //this.dataDaily = {description:'', day:'',date: '',  time:'' };
+  
  
     updateDay(day){
       this.dataDaily.day = day;
     }
 }
+
+// trigger: { every: { hour: this.dataDaily.time, }, count: 1 },
